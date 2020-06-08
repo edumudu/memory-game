6613bc2b51081c0ca2d4e29ea471762ef67abbc7
Eduardo Wesley <eduardomudutiu@gmail.com>
2020-06-08 17:35:54 -0300

    create alert when player left of room

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -7,7 +7,6 @@ import socket from './io';
 const el = document.querySelector('#board');
 const modal = new Modal('#modal');
 const menu = new Modal('#menu');
-const playerTurn = document.querySelector('#player-turn');
 
 let game;
 let room;
@@ -15,55 +14,68 @@ let room;
 menu.setAction('<i class="fas fa-play"></i>', () => {
   socket.emit('room');
   menu.setTitle('Waiting for other player')
-  menu.removeAction(0);
+  menu.disableAction(0);
 });
 menu.show();
 
-modal.setAction('<i class="fas fa-play"></i>', () => {
+modal.setAction('<i class="fas fa-undo-alt"></i>', () => {
   socket.emit('room', room);
   modal.hide();
   menu.show();
   game.destroy();
-  playerTurn.textContent = '';
-}, ['success'], 'Rematch');
+}, 'success', 'Rematch');
+
+modal.setAction('<i class="fas fa-play"></i>', () => {
+  socket.emit('player-left');
+  socket.emit('room');
+  modal.hide();
+  menu.show();
+  game.destroy();
+}, '', 'New game');
 
 socket.on('start-game', (board, playerRoom) => {
   room = playerRoom;
   game = new Board(el, board, socket.id);
+  game.setPlayerTurn(board.playerOfTheTime);
   menu.hide();
-  playerTurn.textContent = board.playerOfTheTime === game.me ? 'Your turn' : 'Enemy turn';
-})
 
-socket.on('check', ids => {
-  game.check(ids);
-})
+  socket.on('enemy-left', () => {
+    game.destroy();
+    modal.hide();
+    menu.setTitle('Enemy left the room');
+    menu.enableAction(0);
+    menu.show();
+  })
 
-socket.on('flip', id => {
-  game.flip(id);
-});
-
-socket.on('unflip', ids => {
-  setTimeout(() => game.unflip(ids), 800)
-});
-
-socket.on('hits', scoreboard => {
-  game.setScoreboard(scoreboard);
-});
-
-socket.on('won', () => {
-  game.stopTimers();
-  modal.setTitle('You win!');
-  modal.show();
-  socket.emit('leave', room);
-});
-
-socket.on('lose', () => {
-  game.stopTimers();
-  modal.setTitle('You lose!');
-  modal.show();
-  socket.emit('leave', room);
-});
-
-socket.on('toggle-player', player => {
-  playerTurn.textContent = player === game.me ? 'Your turn' : 'Enemy turn';
+  socket.on('check', ids => {
+    game.check(ids);
+  })
+  
+  socket.on('flip', id => {
+    game.flip(id);
+  });
+  
+  socket.on('unflip', ids => {
+    setTimeout(() => game.unflip(ids), 800)
+  });
+  
+  socket.on('hits', scoreboard => {
+    game.setScoreboard(scoreboard);
+  });
+  
+  socket.on('won', () => {
+    game.stopTimers();
+    modal.setTitle('You win!');
+    modal.show();
+  });
+  
+  socket.on('lose', () => {
+    game.stopTimers();
+    modal.setTitle('You lose!');
+    modal.show();
+  });
+  
+  socket.on('toggle-player', player => {
+    game.setPlayerTurn(player);
+  })
 })
